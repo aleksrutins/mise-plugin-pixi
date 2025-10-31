a3882defa4a94d49cb11a7cf548f12c5e0caaa82
Aleks Rutins <keeper@farthergate.com>
2025-10-31 09:43:53 -0400

    Fill in some stuff

--- metadata.lua
+++ metadata.lua
@@ -4,26 +4,27 @@
 
 PLUGIN = { -- luacheck: ignore
     -- Required: Plugin name (will be the backend name users reference)
-    name = "<BACKEND>",
+    name = "pixi",
 
     -- Required: Plugin version (not the tool versions)
     version = "1.0.0",
 
     -- Required: Brief description of the backend and tools it manages
-    description = "A mise backend plugin for <BACKEND> tools",
+    description = "A mise backend plugin for Pixi and Prefix.dev",
 
     -- Required: Plugin author/maintainer
-    author = "<GITHUB_USER>",
+    author = "aleksrutins",
 
     -- Optional: Plugin homepage/repository URL
-    homepage = "https://github.com/<GITHUB_USER>/<BACKEND>",
+    homepage = "https://github.com/aleksrutins/mise-plugin-pixi",
 
     -- Optional: Plugin license
     license = "MIT",
 
     -- Optional: Important notes for users
     notes = {
-        -- "Requires <BACKEND> to be installed on your system",
-        -- "This plugin manages tools from the <BACKEND> ecosystem"
+        -- "Requires pixi to be installed on your system",
+        -- "This plugin manages tools from the pixi ecosystem"
+        "Requires Pixi to be installed; this can be done with `mise use pixi`."
     },
 }
